02635949beb0ab565bd88c7e3fd0a5e97670f546
Josias Fraga <josiasrs2009@gmail.com>
2023-10-13 10:04:42 -0300

    hotfixes

--- plugins/Localization/config/routes.php
+++ plugins/Localization/config/routes.php
@@ -1,18 +1,15 @@
 <?php
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
-Router::plugin(
-    'Localization',
-    ['path' => '/localization'],
-    function (RouteBuilder $routes) {
-    	// Extension JSON
-		$routes->setExtensions(['json']);
-
-		// Extend resources
-		$routes->resources('Cidades');
-
-        $routes->fallbacks(DashedRoute::class);
-    }
-);
+return static function (RouteBuilder $routes) {
+    $routes->plugin(
+        'Localization',
+        ['path' => '/localization'],
+        function (RouteBuilder $builder) {
+            $builder->setExtensions(['json']);
+            $builder->resources('Cidades');
+            $builder->fallbacks(DashedRoute::class);
+        }
+    );
+};
